6b9b4de9359d6ca91083f26152dc2d2f93a16944
Tim Caswell <tim@creationix.com>
2011-08-26 23:25:29 -0700

    Get a card rendering on the TouchPad (with modified jail config)

--- makeipk.sh
+++ makeipk.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
 echo "Copying game logic into webOS package"
-mv webos/node_modules/engine.js webos/node_modules/engine.js.bak
+mv webos/node_modules/platform.js webos/node_modules/platform.js.bak
 cp -r lib/* webos/node_modules
-mv webos/node_modules/engine.js.bak webos/node_modules/engine.js
+mv webos/node_modules/platform.js.bak webos/node_modules/platform.js
 echo "Copying game resources into webOS package"
-mkdir webos/res
+mkdir -p webos/res
 cp -r www/game/res/* webos/res/
-echo "Packaging webOS application"
 palm-package webos
-echo "Installing to device"
 palm-install com.creationix.minimason_0.0.0_all.ipk
-echo "Running App"
 palm-launch com.creationix.minimason
 echo "Done.."
