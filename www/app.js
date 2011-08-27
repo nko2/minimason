893473664ce79cff59874470bd3afcab2e55aa75
Tim Caswell <tim@creationix.com>
2011-08-26 18:03:37 -0700

    Use cross-platform library structure

--- www/app.js
+++
@@ -1,9 +0,0 @@
-var canvas, gl, context;
-
-window.addEventListener('load', function (evt) {
-
-  document.body.textContent = "";
-  canvas = document.createElement('canvas');
-  document.body.appendChild(canvas);
-
-}, true);
