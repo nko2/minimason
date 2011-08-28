edaf78fcb122e22ec4e2134786cf9bb747e2afc9
Tim Caswell <tim@creationix.com>
2011-08-27 20:01:57 -0700

    Sync changes

--- makeipk.sh
+++ makeipk.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 echo "Copying game logic into webOS package"
-mv webos/node_modules/platform.js webos/node_modules/platform.js.bak
-cp -r lib/* webos/node_modules
-mv webos/node_modules/platform.js.bak webos/node_modules/platform.js
-echo "Copying game resources into webOS package"
-mkdir -p webos/res
-cp -r www/game/res/* webos/res/
-palm-package webos
+mkdir -p ipk-staging/node_modules
+cp -r lib/* ipk-staging/node_modules/
+cp webos/node_modules/platform.js ipk-staging/node_modules/
+cp -r arm-modules/*.js arm-modules/*.node ipk-staging/node_modules/
+cp arm-modules/node-arm ipk-staging/main
+cp webos/*.json webos/*.js ipk-staging/
+cp -r webos/res ipk-staging/
+
+palm-package ipk-staging
 palm-install com.creationix.minimason_0.0.0_all.ipk
 mkdir -p ipkgs
 mv com.creationix.minimason_0.0.0_all.ipk www/ipkgs/
 palm-launch com.creationix.minimason
+rm -rf ipk-staging
 echo "Done.."
